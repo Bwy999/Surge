c9415ebfb1366c68863200f35a2d9fc6040fcd5a
Biao <40375808+iJony@users.noreply.github.com>
2020-07-06 19:35:46 +0800

    Update Colorful.js

--- Script/Colorful.js
+++ Script/Colorful.js
@@ -1,8 +1,6 @@
-let obj = JSON.parse($response.body);
-
-    obj.result.vip_expired_at = 4075798974,
-    obj.result.is_xy_vip = true,
-    obj.result.xy_vip_expire = 0,
-    obj.result.is_vip = true
-
-$done({body: JSON.stringify(obj)});
+let obj = JSON.parse($response.body)
+obj.result.xy_vip_expire = 4070880000
+obj.result.is_vip = true
+obj.result.vip_expired_at = 4070880000
+obj.result.is_xy_vip = true
+$done({body: JSON.stringify(obj)})
