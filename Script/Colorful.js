b70c84cef79923681eb15cfb318997e1d8d66322
Biao <40375808+Aston808@users.noreply.github.com>
2020-08-01 18:26:12 +0800

    Delete Colorful.js

--- Script/Colorful.js
+++
@@ -1,6 +0,0 @@
-let obj = JSON.parse($response.body)
-obj.result.xy_vip_expire = 4070880000
-obj.result.is_vip = true
-obj.result.vip_expired_at = 4070880000
-obj.result.is_xy_vip = true
-$done({body: JSON.stringify(obj)})
